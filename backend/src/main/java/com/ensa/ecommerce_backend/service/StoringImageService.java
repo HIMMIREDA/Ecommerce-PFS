4fc8d4d9e940df49b1a1735dbd3fdbb4f8f6adf2
ELAAFANI <hamza.elaafani.2001@gmail.com>
2023-04-10 19:00:39 +0000

    fixed StoringImageService bugs

--- backend/src/main/java/com/ensa/ecommerce_backend/service/StoringImageService.java
+++ backend/src/main/java/com/ensa/ecommerce_backend/service/StoringImageService.java
@@ -3,21 +3,26 @@ package com.ensa.ecommerce_backend.service;
 import com.ensa.ecommerce_backend.entity.ProductImageEntity;
 import com.ensa.ecommerce_backend.repository.ProductImageRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
 @Transactional
 public class StoringImageService {
 
-    private final ProductImageRepository productImageRepository;
+    private ProductImageRepository productImageRepository;
 
-    private final String FOLDER_PATH= "\\\\wsl$/Ubuntu/home/elaafani/Ecommerce-PFS/backend/src/main/resources/productImages/";
 
     public StoringImageService(ProductImageRepository productImageRepository) {
         this.productImageRepository = productImageRepository;
@@ -25,16 +30,33 @@ public class StoringImageService {
 
 
     public String uploadImageToFileSystem(MultipartFile file) throws IOException {
-        String filePath=FOLDER_PATH+file.getOriginalFilename();
 
-        ProductImageEntity image = productImageRepository.save(ProductImageEntity.builder()
+        // Get the root directory of the project
+        Path rootDir = Paths.get("").toAbsolutePath();
+
+        // Get the resources directory
+        Path resourcesDir = rootDir.resolve("src/main/resources");
+
+        // Get the images directory within the resources directory
+        Path imagesDir = resourcesDir.resolve("images");
+
+        // Create the images directory if it doesn't exist
+        if (!Files.exists(imagesDir)) {
+            Files.createDirectories(imagesDir);
+        }
+
+        // Save the uploaded file to the images directory
+        Path filePath = imagesDir.resolve(file.getOriginalFilename());
+        Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+
+        // Save the file information to the database
+        productImageRepository.save(ProductImageEntity.builder()
                 .name(file.getOriginalFilename())
                 .type(file.getContentType())
-                .imagePath(filePath).build());
+                .imagePath(filePath.toString())
+                .build());
 
-        file.transferTo(new File(filePath));
-
-        return "file uploaded successfully : " + filePath;
+        return "File uploaded successfully: " + filePath.toString();
     }
 
 

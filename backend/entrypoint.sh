6bd1e64e50f32e5e39ab9b1064f56a119751bab0
reda <reda.himmi@edu.uca.ma>
2023-04-28 23:14:37 +0000

    modified backend container entrypoint.sh file

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,5 +3,5 @@
 ./mvnw spring-boot:run &
 
 while true; do
-    inotifywait -e modify,create,delete,move -r ./src/ && ./mvnw compile
+    inotifywait -e modify,create,delete,move -r ./src/ && eval "kill $(jobs -p) " ; eval "./mvnw spring-boot:run &" 
 done

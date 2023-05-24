3cf51537d143efd6f3e38e5983e8ee2f8d18f722
HIMMIREDA <redahimmi05@gmail.com>
2023-05-24 21:17:58 +0100

    fix: fixed redirection to home after logout

--- client/src/components/common/PrivateRoute.tsx
+++ client/src/components/common/PrivateRoute.tsx
@@ -3,7 +3,6 @@ import { useAppSelector } from "../../app/hooks";
 
 function PrivateRoute() {
   const { user } = useAppSelector((state) => state.auth);
-  
   return user ? <Outlet /> : <Navigate to="/login" />;
 }
 
